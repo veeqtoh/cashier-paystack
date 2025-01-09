39d77124e0f09c6e4b9f65679bf4ff7b28a79a6c
Victor Ukam <victorjohnukam@gmail.com>
2025-01-09 20:37:29 +0000

    Set up facade

---
+++ src/Facades/CashierPaystack.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Veeqtoh\CashierPaystack\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use RuntimeException;
+
+/**
+ * class CashierPaystack
+ * This class provides the facade for this library.
+ *
+ * @package Veeqtoh\CashierPaystack\Facades
+ */
+class CashierPaystack extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @throws RuntimeException
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'cashier-paystack';
+    }
+}
