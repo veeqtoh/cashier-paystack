50bfe8b906fa26e6a051a74f0e45f070d5b8a6a4
Victor Ukam <victorjohnukam@gmail.com>
2025-01-11 23:12:48 +0000

    Update paystack-config.php

--- config/paystack-cashier.php
+++ config/paystack-cashier.php
@@ -6,28 +6,28 @@ return [
     | Public Key From Paystack Dashboard.
     |--------------------------------------------------------------------------
     */
-    'publicKey' => env('PAYSTACK_PUBLIC_KEY'),
+    'publicKey' => env('PAYSTACK_PUBLIC_KEY', ''),
 
     /*
     |--------------------------------------------------------------------------
     | Secret Key From Paystack Dashboard.
     |--------------------------------------------------------------------------
     */
-    'secretKey' => env('PAYSTACK_SECRET_KEY'),
+    'secretKey' => env('PAYSTACK_SECRET_KEY', ''),
 
     /*
     |--------------------------------------------------------------------------
     | Paystack payment URL.
     |--------------------------------------------------------------------------
     */
-    'paymentUrl' => env('PAYSTACK_PAYMENT_URL'),
+    'paymentUrl' => env('PAYSTACK_PAYMENT_URL', 'https://api.paystack.co'),
 
     /*
     |--------------------------------------------------------------------------
     | Optional email address of the merchant.
     |--------------------------------------------------------------------------
     */
-    'merchantEmail' => env('MERCHANT_EMAIL'),
+    'merchantEmail' => env('MERCHANT_EMAIL', ''),
 
     /*
     |--------------------------------------------------------------------------
